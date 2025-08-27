97818024db32c422731ef21fea23febf98182339
Diones <pereiradiones987@gmail.com>
2025-08-27 11:45:42 -0300

    buscando todos os usuários

--- src/main/java/br/com/diones/rest_api_spring_boot/controllers/PersonController.java
+++ src/main/java/br/com/diones/rest_api_spring_boot/controllers/PersonController.java
@@ -1,20 +1,29 @@
 package br.com.diones.rest_api_spring_boot.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.diones.rest_api_spring_boot.models.Person;
 import br.com.diones.rest_api_spring_boot.services.PersonService;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
   
   @Autowired
   private PersonService service;
 
-  @GetMapping("/person/{id}")
+  @GetMapping()
+  public List<Person> findAll() {
+    return service.findAll();
+  }
+
+  @GetMapping("{id}")
   public Person findById(@PathVariable("id") String id) {
     return service.findById(id);
   }
--- src/main/java/br/com/diones/rest_api_spring_boot/services/PersonService.java
+++ src/main/java/br/com/diones/rest_api_spring_boot/services/PersonService.java
@@ -1,10 +1,10 @@
 package br.com.diones.rest_api_spring_boot.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
-
 import org.springframework.stereotype.Service;
-
 import br.com.diones.rest_api_spring_boot.models.Person;
 
 @Service
@@ -13,15 +13,37 @@ public class PersonService {
   private final AtomicLong counter = new AtomicLong();
   private Logger logger = Logger.getLogger(PersonService.class.getName());
 
+  public List<Person> findAll() {
+    logger.info("Buscando uma lista de pessoas!");
+
+    List<Person> persons = new ArrayList<Person>();
+
+    for (int i = 0; i < 8; i++) {
+      Person person = mockPerson(i);
+      persons.add(person);
+    }
+
+    return persons;
+  }
 
   public Person findById(String id) {
-    logger.info("Finding one Person!");
+    logger.info("Buscando uma pessoa!");
 
-    Person person = new Person();    
+    Person person = new Person();
     person.setId(counter.incrementAndGet());
     person.setFirstName("Diones");
     person.setLastName("Santos");
     person.setAddress("Fortaleza - CE - Brasil");
+    person.setGender("Male");
+    return person;
+  }
+  
+  public Person mockPerson(int i) {
+    Person person = new Person();
+    person.setId(counter.incrementAndGet());
+    person.setFirstName("Person Name " + i);
+    person.setLastName("Last Name " + i);
+    person.setAddress("Some Address in Brasil " + i);
     person.setGender("Male");
     return person;
   }
