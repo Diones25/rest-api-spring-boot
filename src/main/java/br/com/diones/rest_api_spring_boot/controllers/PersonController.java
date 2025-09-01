2535de61a4c87fb7b8b70992666a0e11b438181d
Diones <pereiradiones987@gmail.com>
2025-09-01 12:31:03 -0300

    atualizando controller

--- src/main/java/br/com/diones/rest_api_spring_boot/controllers/PersonController.java
+++ src/main/java/br/com/diones/rest_api_spring_boot/controllers/PersonController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 
+import br.com.diones.rest_api_spring_boot.dto.PersonDTO;
+import br.com.diones.rest_api_spring_boot.mapper.PersonMapper;
 import br.com.diones.rest_api_spring_boot.models.Person;
 import br.com.diones.rest_api_spring_boot.services.PersonService;
 
@@ -23,25 +25,29 @@ public class PersonController {
   
   @Autowired
   private PersonService service;
+  private PersonMapper personMapper;
 
   @GetMapping()
-  public List<Person> findAll() {
-    return service.findAll();
-  }
+public List<Person> findAll() {
+  List<PersonDTO> dtos = service.findAll();
+  return personMapper.toEntityList(dtos);
+}
 
   @GetMapping("{id}")
   public Person findById(@PathVariable("id") Long id) {
-    return service.findById(id);
+    return personMapper.toEntity(service.findById(id));
   }
 
   @PostMapping()
   public Person create(@RequestBody Person person) {
-    return service.create(person);
+    PersonDTO personDTO = personMapper.toDTO(person);
+    return personMapper.toEntity(service.create(personDTO));
   }
 
   @PutMapping()
   public Person update(@RequestBody Person person) {
-    return service.update(person);
+    PersonDTO personDTO = personMapper.toDTO(person);
+    return personMapper.toEntity(service.update(personDTO));
   }
 
   @DeleteMapping("{id}")
