1bce53838f57916122d202b47f70d1d83928cc74
Diones <pereiradiones987@gmail.com>
2025-09-01 12:22:05 -0300

    adicionando DTO

---
+++ src/main/java/br/com/diones/rest_api_spring_boot/dto/PersonDTO.java
@@ -0,0 +1,33 @@
+package br.com.diones.rest_api_spring_boot.dto;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PersonDTO implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+
+  @NotBlank(message = "O primeiro nome é obrigatório")
+  @Size(max = 80, message = "O primeiro nome deve ter no máximo 80 caracteres")
+  private String firstName;
+
+  @NotBlank(message = "O sobrenome é obrigatório")
+  @Size(max = 80, message = "O sobrenome deve ter no máximo 80 caracteres")
+  private String lastName;
+
+  @NotBlank(message = "O endereço é obrigatório")
+  @Size(max = 100, message = "O endereço deve ter no máximo 100 caracteres")
+  private String address;
+
+  @NotBlank(message = "O gênero é obrigatório")
+  @Size(max = 6, message = "O gênero deve ter no máximo 6 caracteres")
+  private String gender;
+
+  public PersonDTO() {
+  }
+}
