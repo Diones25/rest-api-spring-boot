4ac90d5031bc0f0e9cef41a33745afd4caec50db
Diones <pereiradiones987@gmail.com>
2025-09-01 12:21:33 -0300

    atualizando service

--- src/main/java/br/com/diones/rest_api_spring_boot/services/PersonService.java
+++ src/main/java/br/com/diones/rest_api_spring_boot/services/PersonService.java
@@ -4,56 +4,68 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.diones.rest_api_spring_boot.controllers.TesteLogController;
+import br.com.diones.rest_api_spring_boot.dto.PersonDTO;
 import br.com.diones.rest_api_spring_boot.exception.ResourceNotfoundExcetion;
+import br.com.diones.rest_api_spring_boot.mapper.PersonMapper;
 import br.com.diones.rest_api_spring_boot.models.Person;
 import br.com.diones.rest_api_spring_boot.repository.PersonRepository;
 
 @Service
 public class PersonService {
 
-  private Logger logger = LoggerFactory.getLogger(TesteLogController.class);
+  private final Logger logger = LoggerFactory.getLogger(PersonService.class);
 
-  @Autowired
-  private PersonRepository repository;
+  private final PersonRepository repository;
+  private final PersonMapper mapper;
+
+  public PersonService(PersonRepository repository, PersonMapper mapper) {
+    this.repository = repository;
+    this.mapper = mapper;
+  }
 
   public List<Person> findAll() {
     logger.info("Buscando uma lista de pessoas!");
-    return repository.findAll();
+    List<PersonDTO> personsDTO = repository.findAll();
+    return mapper.toEntityList(personsDTO);
   }
 
-  public Person findById(Long id) {
+  public PersonDTO findById(Long id) {
     logger.info("Buscando uma pessoa!");
-    return repository.findById(id)
-      .orElseThrow(() -> new ResourceNotfoundExcetion("Pessoa com o id: "+id+" não encontrada!"));
+    PersonDTO entityDTO = repository.findById(id)
+      .orElseThrow(() -> new ResourceNotfoundExcetion("Pessoa com o id: " + id + " não encontrada!"));
+    Person entity = mapper.toEntity(entityDTO);
+    return mapper.toDTO(entity);
   }
 
-  public Person create(Person person) {
+  public PersonDTO create(PersonDTO dto) {
     logger.info("Criando uma pessoa!");
-    return repository.save(person);
+    Person entity = mapper.toEntity(dto);
+    PersonDTO savedDTO = repository.save(mapper.toDTO(entity));
+    return savedDTO;
   }
 
-  public Person update(Person person) {
+  public PersonDTO update(PersonDTO dto) {
     logger.info("Atualizando uma pessoa!");
 
-    Person entity = repository.findById(person.getId())
-        .orElseThrow(() -> new ResourceNotfoundExcetion("Pessoa com o id: " + person.getId() + " não encontrada!"));
+    Person entity = mapper.toEntity(repository.findById(dto.getId())
+        .orElseThrow(() -> new ResourceNotfoundExcetion("Pessoa com o id: " + dto.getId() + " não encontrada!"));
 
-    entity.setFirstName(person.getFirstName());
-    entity.setLastName(person.getLastName());
-    entity.setAddress(person.getAddress());
-    entity.setGender(person.getGender());
-        
-    return repository.save(person);
+    // Atualiza apenas os campos que podem ser alterados
+    entity.setFirstName(dto.getFirstName());
+    entity.setLastName(dto.getLastName());
+    entity.setAddress(dto.getAddress());
+    entity.setGender(dto.getGender());
+
+    Person updated = repository.save(entity);
+    return mapper.toDTO(updated);
   }
 
   public void delete(Long id) {
     logger.info("Deletando uma pessoa!");
-    Person entity =repository.findById(id)
-        .orElseThrow(() -> new ResourceNotfoundExcetion("Pessoa com o id: " + id + " não encontrada!"));
+    Person entity = mapper.toEntity(repository.findById(id)
+        .orElseThrow(() -> new ResourceNotfoundExcetion("Pessoa com o id: " + id + " não encontrada!"));
 
     repository.delete(entity);
   }
