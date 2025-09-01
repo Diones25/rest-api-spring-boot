8864f065a1a5a8c10b7ca0c9c445e38b2580276a
Diones <pereiradiones987@gmail.com>
2025-09-01 17:00:02 -0300

    ajustando mapper

--- src/main/java/br/com/diones/rest_api_spring_boot/mapper/PersonMapper.java
+++ src/main/java/br/com/diones/rest_api_spring_boot/mapper/PersonMapper.java
@@ -8,6 +8,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
   Person toEntity(PersonRequestDTO dto);
-
-  PersonResponseDTO toResponseDTO(Person produto);
+  PersonResponseDTO toResponseDTO(Person person);
 }
