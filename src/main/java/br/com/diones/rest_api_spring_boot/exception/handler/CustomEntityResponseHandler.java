0ad0ebd9944bb660b2251ec23f225f733b5c2d65
Diones <pereiradiones987@gmail.com>
2025-09-01 22:02:13 -0300

    sobrescrevendo tratamento de exceção

--- src/main/java/br/com/diones/rest_api_spring_boot/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/br/com/diones/rest_api_spring_boot/exception/handler/CustomEntityResponseHandler.java
@@ -1,9 +1,15 @@
 package br.com.diones.rest_api_spring_boot.exception.handler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +43,19 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
     );
 
     return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+  }
+  
+  // Sobrescrevendo o tratamento de validação
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+      errors.put(error.getField(), error.getDefaultMessage());
+    }
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 }
